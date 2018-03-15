426c54beea688b8327c0d5079d588e2e83bedbe2
George "Tre" Ammatuna <tretuna@gmail.com>
2018-03-14 17:31:32 -0700

    Update readme and colors

--- README.md
+++ README.md
@@ -2,14 +2,34 @@
 
 > Avvo UI Kit React component library
 
-[![NPM](https://img.shields.io/npm/v/suitably-react.svg)](https://www.npmjs.com/package/suitably-react) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
+[![NPM](https://img.shields.io/npm/v/suitably-react.svg)](https://www.npmjs.com/package/suitably-react)
 
 ## Install
 
+If you don't have Yarn isntalled already:
+
 ```bash
+brew install yarn
+```
+
+Install into your React application
+
+```bash
+npm install --save suitably-react
+// or
 yarn add suitably-react
 ```
 
+## Using this repo in your React application while contributing
+
+* Clone the repo and follow installation instructions for contributing.
+* From the repo root directory run `npm link`
+* From your application's root directory (where your package.json is) run `npm init suitably-react`
+
+This will create a symlink between the repo on your machine and your application.
+Any additions you make will be able to be consumed by your application.
+Afterwards, follow the `NPM publishing` directions for
+
 ## License
 
-MIT Â© [Avvo](https://github.com/avvo)
+MIT © [Avvo](https://github.com/avvo)
--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,4 +1,44 @@
 module.exports = {
+  // Avvo Primary
+  navy: "#00447B",
+  electricBlue: "#26DDFC",
+  teal: "#00B7AF",
+  coral: "#F27760",
+  coolGrey: "#CDCFCE",
+  bgColor: "#f9f9f9",
+  // Avvo secondary
+  blue: "#008CC9",
+  orange: "#F55D25",
+  // Avvo Tertiary
+  tertiaryGreen: "#52A304",
+  tertiaryYellow: "#FC9835",
+  tertiaryRed: "#ED4F4B",
+  // grey scale
+  white: "#FFFFFF",
+  grey10: "#F9F9F9",
+  grey20: "#EAEAEA",
+  grey40: "#CCCCCC",
+  grey50: "#969696",
+  grey70: "#666666",
+  grey90: "#333333",
+  black: "#000000",
+  // Avvo semantic colors
+  avvoPro: "#26DDFC",
+  semPrimary: "#F55D25",
+  semSecondary: "#0394b9",
+  semSuccess: "#52A304",
+  semInfo: "##03AFDA",
+  semWarning: "#FC9835",
+  semDanger: "#ED4F4B",
+  semActive: "#F55D25",
+  // Avvo active item
+  activeItem: "#FFFFFF",
+  activeBg: "#F55D25",
+  // Avvo feedback states
+  stateSuccess: "rgba(82, 163, 4, 0.03)",
+  stateInfo: "rgba(3, 175, 218, 0.03)",
+  stateWarning: "rgba(252, 152, 53, 0.03)",
+  stateDanger: "rgba(237, 79, 75, 0.03)",
   // light shades
   white: "#FFFFFF",
   snow: "#F9FAFC",
@@ -8,14 +48,14 @@ module.exports = {
   silver: "#8492A6",
   slate: "#3C4858",
   steel: "#273444",
-  black: "#1F2D3D",
+  // black: "#1F2D3D",
   // dark shades
   smoke: "#E0E6ED",
   darkSmoke: "#D3DCE6",
   extraDarkSmoke: "#C0CCDA",
   // blue shades
   lightBlue: "#85D7FF",
-  blue: "#1FB6FF",
+  // blue: "#1FB6FF",
   darkBlue: "#009EEB",
   // purple shades
   lightPurple: "#A389F4",
@@ -27,7 +67,7 @@ module.exports = {
   darkPink: "#FF16D1",
   // orange shades
   lightOrange: "#FF9E7C",
-  orange: "#FF7849",
+  // orange: "#FF7849",
   darkOrange: "#FF5216",
   // green shades
   lightGreen: "#29EB7F",
@@ -36,10 +76,5 @@ module.exports = {
   // yellow shades
   lightYellow: "#FFD55F",
   yellow: "#FFC82C",
-  darkYellow: "#F8B700",
-  // ui colors
-  info: "#1FB6FF",
-  success: "#13CE66",
-  danger: "#FF4949",
-  warning: "#FFC82C"
+  darkYellow: "#F8B700"
 };
